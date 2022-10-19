1457a1569e404e0b382d214cfe6998601ae8a2e6
Matias Rivas <matias.rivas.aa@gmail.com>
2022-10-18 21:11:41 -0300

    Finish

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ while True:
     connection_socketTCP, new_address = server_socketTCP.accept()
     while encoded_message is not None:
         complete_message += encoded_message
-        encoded_message = connection_socketTCP.recv(16, mode=1)
+        encoded_message = connection_socketTCP.recv(16, mode=2)
 
     print(complete_message.decode())
     
